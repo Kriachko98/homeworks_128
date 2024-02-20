5c8302b9c746e7be5162a7c0e3a01d574a9b2adf
Yelyzaveta Kriachko <lizakryachko@gmail.com>
2024-02-21 01:10:08 +0200

    Finished map

--- js-exam/assets/js/dist/main.dev.js
+++ js-exam/assets/js/dist/main.dev.js
@@ -1,8 +1,20 @@
 "use strict";
 
-var map = L.map('map').setView([40.678930, -73.906647], 13);
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OSM</a> contributors'
-}).addTo(map);
-L.marker([40.678930, -73.906647]).addTo(map); // .bindPopup('A pretty CSS popup.<br> Easily customizable.')
-// .openPopup();
+function initMap(link) {
+  link.remove();
+  var map = L.map('map').setView([40.662017, -73.880101], 13);
+  var ownIcon = L.icon({
+    iconUrl: '../images/Pin-min.png',
+    iconSize: [106, 106],
+    // size of the icon
+    iconAnchor: [22, 94] // point of the icon which will correspond to marker's location
+
+  });
+  L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OSM</a> contributors'
+  }).addTo(map);
+  L.marker([40.678930, -73.906647], {
+    icon: ownIcon
+  }).addTo(map); // .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+  // .openPopup();
+}
--- js-exam/assets/js/main.js
+++ js-exam/assets/js/main.js
@@ -1,9 +1,18 @@
-const map = L.map('map').setView([40.678930, -73.906647], 13);
+function initMap (link) {
+    link.remove();
+    const map = L.map('map').setView([40.662017, -73.880101], 13);
+    const ownIcon = L.icon({
+        iconUrl: '../images/Pin-min.png',
+    
+        iconSize:     [106, 106], // size of the icon
+        iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+    });
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OSM</a> contributors'
-}).addTo(map);
+    L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OSM</a> contributors'
+    }).addTo(map);
 
-L.marker([40.678930, -73.906647]).addTo(map)
-    // .bindPopup('A pretty CSS popup.<br> Easily customizable.')
-    // .openPopup();
+    L.marker([40.678930, -73.906647], {icon: ownIcon}).addTo(map)
+        // .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+        // .openPopup();
+}
