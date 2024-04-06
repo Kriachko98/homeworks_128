3dfa64ad4c69a1109fdc187a2a6349e5ecced6d9
Yelyzaveta Kriachko <lizakryachko@gmail.com>
2024-04-06 15:43:43 +0300

    Changed map cover

--- Final project/assets/js/contact.js
+++ Final project/assets/js/contact.js
@@ -7,21 +7,21 @@ function initMap(link) {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OSM</a>'
     }).addTo(map);
 
-    // const customMarker = L.icon({
-    //     iconUrl: 'map-marker.png',
-    //     // shadowUrl: 'leaf-shadow.png',
+    const customMarker = L.icon({
+        iconUrl: 'assets/images/map-marker.png',
+        // shadowUrl: 'leaf-shadow.png',
     
-    //     iconSize:     [38, 95], // size of the icon
-    //     // shadowSize:   [50, 64], // size of the shadow
-    //     iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
-    //     // shadowAnchor: [4, 62],  // the same for the shadow
-    //     // popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
-    // });
+        iconSize:     [36, 36], // size of the icon
+        // shadowSize:   [50, 64], // size of the shadow
+        iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+        // shadowAnchor: [4, 62],  // the same for the shadow
+        // popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+    });
 
-    L.marker([39.796931, -86.063576]).addTo(map)
+    L.marker([39.796931, -86.063576],{icon: customMarker}).addTo(map)
         // .bindPopup('A pretty CSS popup.<br> Easily customizable.')
         // .openPopup();
 }
 
-// , {icon: customMarker}
+
 
