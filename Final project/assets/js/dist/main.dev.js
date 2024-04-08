495f90f1a52ba8b121b12382d7563401794c4bf0
Yelyzaveta Kriachko <lizakryachko@gmail.com>
2024-04-08 19:36:09 +0300

    Validation form finished

--- Final project/assets/js/dist/main.dev.js
+++ Final project/assets/js/dist/main.dev.js
@@ -8,4 +8,16 @@ document.getElementById('hamb--btn').addEventListener('click', function () {
 });
 document.getElementById('hamb--btn--mobile').addEventListener('click', function () {
   document.body.classList.toggle('open--mobile--menu');
+}); // Video
+
+document.addEventListener('DOMContentLoaded', function () {
+  var videoLink = document.querySelector('.about--video--content .video');
+  videoLink.addEventListener('click', function (event) {
+    event.preventDefault();
+    var videoContainer = document.createElement('div');
+    videoContainer.classList.add('video-container');
+    videoContainer.innerHTML = "\n            <div class=\"video-responsive\">\n            <iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/3VMz06iVzqs?si=bpLP8CbA6kSiOgnq\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" referrerpolicy=\"strict-origin-when-cross-origin\" allowfullscreen></iframe>                \n            </div>\n        "; // Додати контейнер в кінець body
+
+    document.body.appendChild(videoContainer);
+  });
 });
--- Final project/assets/js/main.js
+++ Final project/assets/js/main.js
@@ -11,3 +11,24 @@ document.getElementById('hamb--btn--mobile').addEventListener('click', function(
     document.body.classList.toggle('open--mobile--menu')
 })
 
+// Video
+document.addEventListener('DOMContentLoaded', function() {
+ 
+    const videoLink = document.querySelector('.about--video--content .video');
+        
+    videoLink.addEventListener('click', function(event) {
+        event.preventDefault();
+            
+        const videoContainer = document.createElement('div');
+        videoContainer.classList.add('video-container');
+            
+        videoContainer.innerHTML = `
+            <div class="video-responsive">
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/3VMz06iVzqs?si=bpLP8CbA6kSiOgnq" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>                
+            </div>
+        `;
+            
+            // Додати контейнер в кінець body
+            document.body.appendChild(videoContainer);
+        });
+    });
