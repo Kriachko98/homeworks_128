c5cfc2a86dcea3271a90a89a918a3fe86905c9c6
Yelyzaveta Kriachko <lizakryachko@gmail.com>
2024-04-09 16:28:22 +0300

    RWD of about us section is finished

--- Final project/assets/js/dist/slider.dev.js
+++ Final project/assets/js/dist/slider.dev.js
@@ -8,6 +8,31 @@ $(function () {
     auto: true,
     loop: true,
     pause: 3000,
-    controls: false
+    controls: false,
+    responsive: [{
+      breakpoint: 1245,
+      settings: {
+        slideMargin: 70,
+        item: 5
+      }
+    }, {
+      breakpoint: 1060,
+      settings: {
+        slideMargin: 70,
+        item: 4
+      }
+    }, {
+      breakpoint: 835,
+      settings: {
+        slideMargin: 40,
+        item: 3
+      }
+    }, {
+      breakpoint: 545,
+      settings: {
+        slideMargin: 10,
+        item: 2
+      }
+    }]
   });
 });
